cecec2301a0a6edadba0ba1d84e2757cc6498397
Muhammad Tsany Qudsi <tsanyqudsi@gmail.com>
2022-03-14 18:28:25 +0700

    clean up and fix 404

--- workspaces/website/src/templates/404.tsx
+++ workspaces/website/src/templates/404.tsx
@@ -1,16 +1,25 @@
 import { getImage, getStyle } from "../../libs";
-import { stylesAtom } from "@stores/assets";
+import { stylesAtom, imagesAtom } from "@stores/assets";
+import Nav from "@components/Nav";
+
 export default () => {
   return (
-    <main class={getStyle("404", stylesAtom).container}>
-      404
-      <a
-        onClick={() => {
-          history.back();
-        }}
-      >
-        ads
-      </a>
-    </main>
+    <>
+      <Nav />
+      <main class={getStyle("404", stylesAtom).container}>
+        <img src={getImage("error404.png", imagesAtom)} />
+        <a
+          onClick={() => {
+            history.back();
+          }}
+          class={getStyle("404", stylesAtom).back}
+        >
+          Go Back
+        </a>
+        <a href="https://www.flaticon.com/free-icons/404" title="404 icons">
+          404 icons created by Freepik - Flaticon
+        </a>
+      </main>
+    </>
   );
 };
